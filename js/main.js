e15270f69a2b3d0e1437bfd5f79cf315869c40a3
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-09-30 22:56:36 -0400

    feature: added functionailty to clicking rows in table and to the modal
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+const api = `https://movie-api.cyclic.app`;
 let page = 1,
   perPage = 10;
+const noValue = 'N/A';
 
 const createTableRows = (data) => {
   return new Promise((resolve, reject) => {
@@ -9,8 +11,8 @@ const createTableRows = (data) => {
           `<tr data-id="${movie._id}">
               <td>${movie.year}</td>
               <td>${movie.title}</td>
-              <td>${movie.plot ? movie.plot : `N/A`}</td>
-              <td>${movie.rated ? movie.rated : `N/A`}</td>
+              <td>${movie.plot ? movie.plot : noValue}</td>
+              <td>${movie.rated ? movie.rated : noValue}</td>
               <td>${`${Math.floor(movie.runtime / 60)}.${(movie.runtime % 60).toString().padStart(2, '0')}`}</td>
           </tr>`
       )
@@ -23,7 +25,37 @@ const createTableRows = (data) => {
 };
 
 const populateTableRows = (tableRows) => {
+  // populating the table with the string of table rows held by the parameter
   document.querySelector('#moviesTable tbody.tableBody').innerHTML = tableRows;
+
+  // targetting each row in the table
+  document.querySelectorAll('#moviesTable tbody.tableBody tr').forEach((row) => {
+    // adding a 'click' event to each row in the table
+    row.addEventListener('click', (event) => {
+      // fetching the movie object for the movie/row where the 'click' event was triggered
+      fetch(api + `/api/movies/${row.getAttribute('data-id')}`)
+        .then((res) => res.json())
+        .then((data) => {
+          document.querySelector('#detailsModal h5.modal-title').innerHTML = data.title;
+          document.querySelector('#detailsModal div.modal-body').innerHTML = `
+          <img class="img-fluid w-100" src="${data.poster}"><br><br>
+          <strong>Directed By:</strong> ${data.directors.join(', ')}<br><br>
+          <p>${data.fullplot}</p>
+          <strong>Cast:</strong> ${data.cast.length > 0 ? data.cast.join(', ') : noValue}<br><br>
+          <strong>Awards:</strong> ${data.awards.text}<br>
+          <strong>IMDB Rating:</strong> ${data.imdb.rating} (${data.imdb.votes} votes)
+          `;
+        });
+
+      let modal = new bootstrap.Modal(document.getElementById('detailsModal'), {
+        backdrop: 'static', // default true - "static" indicates that clicking on the backdrop will not close the modal window
+        keyboard: false, // default true - false indicates that pressing on the "esc" key will not close the modal window
+        focus: true, // default true - this instructs the browser to place the modal window in focus when initialized
+      });
+
+      modal.show();
+    });
+  });
 };
 
 const updatePagination = (titleSpecified) => {
@@ -37,12 +69,8 @@ const updatePagination = (titleSpecified) => {
 };
 
 const loadMovieData = (title = null) => {
-  // creating api request url
-  const api = `https://movie-api.cyclic.app`;
-  let requestUrl = api + `/api/movies?page=${page}&perPage=${perPage}${title ? `&title=${title}` : ''}`;
-
-  // fetching data from the api, using the api request url
-  fetch(requestUrl)
+  // fetching data from the api, using an api request url
+  fetch(api + `/api/movies?page=${page}&perPage=${perPage}${title ? `&title=${title}` : ''}`)
     .then((res) => res.json())
     .then((data) => {
       createTableRows(data)
